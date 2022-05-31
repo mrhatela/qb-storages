d412c5c330ce93440f53e89e12f417cf28c1a06d
OfekAF <90272994+OfekAF@users.noreply.github.com>
2022-05-31 13:08:05 +0300

    Update storages.sql

--- storages.sql
+++ storages.sql
@@ -3,11 +3,11 @@ CREATE TABLE `storages`
  ENGINE = InnoDB;
 
 
-ALTER TABLE `vaults` ADD COLUMN `storage_size` int NULL DEFAULT 400000 AFTER `storagename`;
+ALTER TABLE `storages` ADD COLUMN `storage_size` int NULL DEFAULT 400000 AFTER `storagename`;
 
-ALTER TABLE `vaults` ADD COLUMN `holders` text NULL DEFAULT NULL AFTER `storage_size`;
+ALTER TABLE `storages` ADD COLUMN `holders` text NULL DEFAULT NULL AFTER `storage_size`;
 
-ALTER TABLE `vaults` ADD COLUMN `storage_location` VARCHAR(255) NULL DEFAULT NULL AFTER `holders`;
+ALTER TABLE `storages` ADD COLUMN `storage_location` VARCHAR(255) NULL DEFAULT NULL AFTER `holders`;
 
-ALTER TABLE `vaults` ADD COLUMN `id` int(11) AFTER `holders`;
-ALTER TABLE `vaults` MODIFY `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE `storages` ADD COLUMN `id` int(11) AFTER `holders`;
+ALTER TABLE `storages` MODIFY `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
